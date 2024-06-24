77449f27b4cb748cfbc6cde729e8f2ccfe69f9c5
4belz <abeldiiaz@gmail.com>
2024-06-24 19:28:46 +0000

    css jss

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Ejemplo de función para manipular el DOM
+document.addEventListener('DOMContentLoaded', function() {
+    // Ejemplo de manipulación del DOM
+    const heading = document.querySelector('h1');
+    heading.textContent = '¡Bienvenido a mi portafolio!';
+});
+
+// Otro código JavaScript aquí
